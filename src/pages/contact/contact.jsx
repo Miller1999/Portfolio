0e0052aaa43c4ba0f1ff186a3602af2bd841dd58
Miller Arias <millerjavierarias@gmail.com>
2024-03-03 22:01:28 -0500

    Fixed: Contact page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -5,11 +5,15 @@ const Contact = () => {
 	return (
 		<main>
 			<h1>Contact</h1>
+			<h2>Say Hello 👋🏽</h2>
+			<p>Hey, wanna talk? Hit me up on WhatsApp for an easy convo!</p>
 			<section className="contact">
 				{social.map((item) => (
 					<div key={item.name}>
-						<h2>{item.name}</h2>
-						<a href={item.link}>{item.name}</a>
+						<a href={item.link}>
+							{item.name}
+							<div className="horizontal"></div>
+						</a>
 					</div>
 				))}
 			</section>
